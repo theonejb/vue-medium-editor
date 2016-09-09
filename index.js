d83c322d935040a003e2098ae80252a1654bbd1f
Jibran <surfer.a1@gmail.com>
2016-09-10 00:45:49 +0400

    Fixed bug and renamed prop

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ module.exports = (function () {
   var Vue = require("Vue");
 
   var MediumEditorComponent = Vue.extend({
-    template: "<div><textarea v-model=\"body\"></textarea></div>",
+    template: "<div><textarea v-model=\"content\"></textarea></div>",
 
-    props: ["data"],
+    props: ["content"],
 
     ready: function () {
       var self = this;
       var MediumEditor = require("medium-editor");
 
       var editorToDataSync = function (data, editable) {
-        self.data = editable.innerHTML;
+        self.content = editable.innerHTML;
       };
 
       this.editor = new MediumEditor(this.$el.children[0]);
       this.editor.subscribe("editableInput", editorToDataSync);
 
       this.$on("data-updated", function () {
-        this.editor.setContent(this.data);
+        this.editor.setContent(this.content);
       });
     }
   });
